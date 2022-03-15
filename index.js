7ca384afaec8597954f4950c3dd4e044422595bb
Natalya <innominatamse@gmail.com>
2022-03-16 00:17:44 +0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     let ulList = document.querySelector('.list');
+    let inputValue = document.querySelector('.input-t');
     let newElement = '';
 
+    //    Add task
     function addText(text) {
         let newElements = document.createElement('li');
         newElement = newElements;
@@ -10,20 +12,47 @@ document.addEventListener('DOMContentLoaded', function () {
         return newElement;
     }
 
+    // Add button remove and event remove element from DOM
     function removeButton() {
         let button = document.createElement('button');
         button.classList.add("button", "button__remove");
-        newElement.append(button)
+        newElement.append(button);
+
+        button.addEventListener('click', function (event) {
+            let {
+                target
+            } = event;
+            target.parentElement.remove();
+        });
+
         return newElement;
     }
 
-    function render(text) {
-        ulList.insertAdjacentElement('beforeEnd', addText(text), removeButton());
-    }
+    // Element rendering in DOM
+    function render(position = 'afterbegin', result) {
+        inputValue.addEventListener('keypress', function (event) {
+            if (event.key !== 'Enter') {
+                return;
+            } else if (event.key == 'Enter' && inputValue.value.length == 0) {
+                return;
+            }
+            ulList.insertAdjacentElement(position, addText(inputValue.value), removeButton());
+            inputValue.value = null;
+        });
+    };
+    render();
 
-    render('task 1');
-    render('task 2');
-    render('task 3');
-    render('task 4');
-    
+    // Transfer the task to the status completed
+    ulList.addEventListener('click', function (event) {
+        event.preventDefault();
+        let {
+            target
+        } = event;
+        if (!target.classList.contains('list__item')) {
+            return
+        };
+        
+        target.classList.add('list__item_completed');
+    });
+
 });
