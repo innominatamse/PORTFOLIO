3304d611f9249c109224e401f79584756e3708ec
Natalya <innominatamse@gmail.com>
2022-03-20 10:07:58 +0300

    Calculator bugs fixed; rewritten function calculate

--- cacl.js
+++ cacl.js
@@ -1,8 +1,8 @@
 let input = document.querySelector('input');
 let calculatorItem = document.querySelector('.calculator__list');
-let firstNumber = null;
+let firstNumber = [];
 let mainOperator = null;
-let result = 0;
+let result = [];
 let inputClear = false;
 
 // function calculate
@@ -29,14 +29,12 @@ let display = function (event) {
     };
 
     if (inputClear) {
-        firstNumber = input.valueAsNumber;
+        firstNumber.push(input.valueAsNumber);
         input.value = null;
         inputClear = false;
     };
 
     input.value += target.dataset.number;
-
-
 };
 
 // function to display DOM result of operations
@@ -45,44 +43,51 @@ calculatorItem.addEventListener('click', function (event) {
     let {
         target
     } = event;
-    if (!target.dataset.operator || target.dataset.operator == "equals") {
+    if (!target.dataset.operator) {
         return;
-    }
-    if (firstNumber) {
-        let firstItems = Number(firstNumber);
-        let secondItems = input.valueAsNumber;
-        let operators = mainOperator;
-        result = calculate(firstItems, secondItems, operators);
-        // return;
-    }
+    };
     inputClear = true;
     mainOperator = target.dataset.operator;
 
-    console.log(mainOperator)
+    if (firstNumber.length == 2) {
+        result = firstNumber.reduce(function (firstItem, secondItem) {
+            let cacl = calculate(+firstItem, +secondItem, mainOperator);
+            firstNumber.splice(0);
+            firstNumber.push(cacl);
+            mainOperator = null;
+            return;
+        });
+    } else {
 
-    // if (target.dataset.operator == "equals") {
-    //     let firstItems = Number(firstNumber);
-    //     let secondItems = input.valueAsNumber;
-    //     let operators = mainOperator;
-    //     result = calculate(firstItems, secondItems, operators);
-    //     console.log(mainOperator);
-    //     return;
-    // };
+    }
+
 });
-
 calculatorItem.addEventListener('click', function (event) {
     let {
         target
     } = event;
-    if (target.dataset.operator !== "equals") {
+    if (target.dataset.result !== "equals") {
         return;
     }
-    if (target.dataset.operator == "equals") {
-        // inputClear = true;
-        let firstItems = Number(firstNumber);
-        let secondItems = input.valueAsNumber;
-        let results = calculate(firstItems, secondItems, mainOperator);
-        input.value = results;
+    if (target.dataset.result == "equals") {
+        if (firstNumber.length == 2) {
+            firstNumber.reduce(function (firstItem, secondItem) {
+                let cacl = calculate(+firstItem, +secondItem, mainOperator);
+                // input.value = cacl;
+                firstNumber.splice(0);
+                firstNumber.push(cacl);
+                return;
+
+            });
+        }
+        let firstItem = +firstNumber[0];
+        let secondItem = input.valueAsNumber;
+
+        let cacl = calculate(firstItem, secondItem, mainOperator);
+        firstNumber.splice(0);
+        input.value = cacl;
+        mainOperator = null;
     }
+
 
 })
